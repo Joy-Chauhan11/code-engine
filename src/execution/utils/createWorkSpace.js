6920d4cb0b734c48ab586a27832751b12de28313
Joy Chauhan <joychauhan1104@gmail.com>
2026-07-25 00:14:57 +0530

    api validation added

--- src/execution/utils/createWorkSpace.js
+++ src/execution/utils/createWorkSpace.js
@@ -1,12 +1,18 @@
 import fs from "fs/promises";
 import path from "path";
 import crypto from "crypto";
+import os from "os";
+
 
 export async function createWorkspace() {
   const id = crypto.randomUUID();
 
-  const workspace = path.join(process.cwd(), "src", "execution", "temp", id);
 
+const workspace = path.join(
+    os.tmpdir(),
+    "engine",
+    id
+);
   await fs.mkdir(workspace, { recursive: true });
 
   return {id,workspace};
---
+++ src/middleware/validateRequest.js
@@ -0,0 +1,16 @@
+export function validateRequest(schema) {
+    return (req, res, next) => {
+        const { error, value } = schema.validate(req.body);
+
+        if (error) {
+            return res.status(400).json({
+                success: false,
+                message: error.details[0].message,
+            });
+        }
+
+        req.body = value;
+
+        next();
+    };
+}
